98fdc2da02c3813ca4bc66b88720e4e38b6b1476
Cody Maggard <codym@everfi.com>
2011-12-20 16:58:08 -0500

    Bumping ruby to 1.9.3

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -26,9 +26,9 @@ echo "Installing RVM (Ruby Version Manager) ..."
   source ~/.bash_profile
 
 
-echo "Installing Ruby 1.9.2 stable and making it the default Ruby ..."
-  rvm install 1.9.2-p290
-  rvm use 1.9.2 --default
+echo "Installing Ruby 1.9.3 stable and making it the default Ruby ..."
+  rvm install 1.9.3
+  rvm use 1.9.3
 
 echo "Installing Bundler for managing Ruby libraries ..."
   gem install bundler --no-rdoc --no-ri
